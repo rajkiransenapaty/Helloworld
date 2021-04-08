5956927fc5a99066953f56c3493ebd506a2477d9
rajkiran senapaty <senapaty.rajkiran6@gmail.com>
2021-04-08 20:24:37 +0530

    Line added to POLL SCM job auto build

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ class hello{
     public static void main(String[] args) {
         System.out.println("Let's start conncet bitbucket to jenkins");
 	System.out.println("Let's this work hopefully for the 3rd time"); 
+	System.out.println("POLL SCM practice");
 	
         
     }
